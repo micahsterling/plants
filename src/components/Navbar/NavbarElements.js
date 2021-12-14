7ea792c9ae3bac503a336b55c0853e8d739e7aa5
Micah Sterling <msterling89@gmail.com>
2021-12-13 18:09:50 -0600

    add styled MobileIcon

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -39,3 +39,17 @@ export const NavLogo = styled(LinkR)`
   text-decorations: none;
 `
 
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+` 
+
