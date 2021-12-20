80be9707d6626b4aaba0bc23a3e89914215a2266
Micah Sterling <msterling89@gmail.com>
2021-12-19 20:08:26 -0600

    add styles heroContainer

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <HeroContainer>
-      
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
     </HeroContainer>
   )
 }
