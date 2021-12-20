55372e2208e7c18bab0f34147ab6da51cf441544
Micah Sterling <msterling89@gmail.com>
2021-12-20 16:36:25 -0600

    add styled HeroH1

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -42,3 +42,16 @@ export const HeroContent = styled.div`
   align-items: center;
 `
 
+export const HeroH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 32px
+  }
+`
