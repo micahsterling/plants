e77a920c6d85fcb0ac92f49947c102809c2d956f
Micah Sterling <msterling89@gmail.com>
2021-12-19 18:25:15 -0600

    add pages index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-import NavBar from './components/Navbar';
 import {BrowserRouter as Router} from 'react-router-dom'
-import Sidebar from './components/Sidebar';
+import Home from './pages';
 
 function App() {
   return (
     <Router>
-      <Sidebar />
-      <NavBar />
+     <Home />
     </Router>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Home = () => {
+  return (
+    <>
+      <Sidebar />
+      <NavBar/ >
+    </>
+  )
+}
+
+export default Home
+
