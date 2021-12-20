bb162d807d833db8cc59cf93430e20519a017cd4
Micah Sterling <msterling89@gmail.com>
2021-12-19 20:13:57 -0600

    add styles heroBg

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -12,3 +12,14 @@ export const HeroContainer = styled.div`
 
   /* add :before styles */
 `
+
+export const HeroBg = styled.div`
+  postition: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`
