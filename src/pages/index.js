c079ea26d30e9fbabff70ba24c64df7499d06437
Micah Sterling <msterling89@gmail.com>
2021-12-23 20:09:27 -0600

    add styled InfoContainer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
 import NavBar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 
@@ -14,6 +15,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle}/>
       <NavBar toggle={toggle}/>
       <HeroSection />
+      <InfoSection />
     </>
   )
 }
