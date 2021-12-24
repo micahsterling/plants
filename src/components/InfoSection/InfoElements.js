840109fd5dc2fa68b2c27ae307d83c76f22d69ab
Micah Sterling <msterling89@gmail.com>
2021-12-24 16:31:16 -0600

    add styled Heading

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -47,7 +47,7 @@ export const TextWrapper = styled.div`
   padding-bottom: 60px;
 
 `
-export const TopLine= styled.p`
+export const TopLine = styled.p`
   color: #01bf71;
   font-size: 16px;
   line-height: 16px;
@@ -55,4 +55,16 @@ export const TopLine= styled.p`
   letter-spacing: 1.4px;
   text-transform: uppercase;
   margin-bottom: 16px;
+`
+
+export const Heading = styled.h1`
+  margin-bottom: 24px;
+  font-size: 48px;
+  line-height: 1.1;
+  font-weight: 600;
+  color: ${({lightText}) => (lightText ? '#f7f8fa' : '010606')};
+
+  @media screen (max-width: 480px) {
+    font-size: 32px;
+  }
 `
