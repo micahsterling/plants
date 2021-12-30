65c4a74c0852e5700b9068c3dbb056205d7e3a6b
Micah Sterling <msterling89@gmail.com>
2021-12-30 17:39:07 -0600

    add homeObjTwo/three

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -37,8 +37,9 @@ const InfoSection = ({
                 offset={-80}
                 primary={primary ? 1 : 0}
                 dark={dark ? 1 : 0}
-                dark2={dark2 ? 1 : 0}
-                >{buttonLable}</Button>
+                dark2={dark2 ? 1 : 0}>
+                  {buttonLable}
+                </Button>
               </BtnWrap>
             </TextWrapper>
             </Column1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/InfoSection'
-import { homeObjOne } from '../components/InfoSection/Data'
+import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
 import NavBar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 
@@ -17,6 +17,8 @@ const Home = () => {
       <NavBar toggle={toggle} />
       <HeroSection />
       <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
     </>
   )
 }
