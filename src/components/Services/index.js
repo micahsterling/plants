0f8db75637f69cddcc2a9c4cc1b99141971d3b0a
Micah Sterling <msterling89@gmail.com>
2021-12-30 18:10:37 -0600

    add services index framework

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <>
-      
-    </>
+    <ServicesContainer id="services">
+      <ServicesH1>Our Services</ServicesH1>
+      <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon1}>
+            <ServicesH1>Service</ServicesH1>
+            <ServicesP>description of services</ServicesP>
+          </ServicesIcon>
+          <ServicesIcon src={Icon2}>
+            <ServicesH1>Service 2</ServicesH1>
+            <ServicesP>description of services</ServicesP>
+          </ServicesIcon>
+          <ServicesIcon src={Icon3}>
+            <ServicesH1>Service 3</ServicesH1>
+            <ServicesP>description of services</ServicesP>
+          </ServicesIcon>
+        </ServicesCard>
+      </ServicesWrapper>
+    </ServicesContainer>
   )
 }
 
