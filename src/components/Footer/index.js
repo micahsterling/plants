f3941eca96bb40e7cc4bf2bdfef85ca77510d8dc
Micah Sterling <msterling89@gmail.com>
2022-01-02 11:13:26 -0600

    add footer framework

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <FooterContainer>
+        <FooterWrap>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinkItems>
+                <FooterLinkTitle>
+                  <FooterLink to="/signin">About us</FooterLink>
+                  <FooterLink to="/signin">About us</FooterLink>
+                </FooterLinkTitle>
+              </FooterLinkItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+        </FooterWrap>
+      </FooterContainer>
+    </>
+  )
+}
+
+export default Footer
