df0207205e5fb649dc1ca9afe762ea675252de39
Micah Sterling <msterling89@gmail.com>
2022-01-03 18:25:38 -0600

    add styled footerLinksWrapper

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -23,3 +23,11 @@ export const FooterLinksContainer = styled.div`
     padding-top: 32px; 
   }
 `
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`
