23b9781d03b0c090feb6a8874b9d4931d2418724
Micah Sterling <msterling89@gmail.com>
2022-01-05 17:42:36 -0600

    add footer imports

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
 import {
   FooterContainer, 
   FooterWrap, 
@@ -6,7 +7,12 @@ import {
   FooterLinksWrapper, 
   FooterLinkItems, 
   FooterLinkTitle, 
-  FooterLink} 
+  FooterLink,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIconLink,
+} 
   from './FooterElements'
 
 const Footer = () => {
@@ -29,6 +35,20 @@ const Footer = () => {
               </FooterLinkItems>
             </FooterLinksWrapper>
           </FooterLinksContainer>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>
+              Plants
+            </SocialLogo>
+            <WebsiteRights>Plants {new Date().getFullYear()} All rights reserved</WebsiteRights>
+            <SocailIcons>
+              <SocialIconLink href="//" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="//www/youtube.com/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+            </SocailIcons>
+          </SocialMediaWrap>
         </FooterWrap>
       </FooterContainer>
     </>
