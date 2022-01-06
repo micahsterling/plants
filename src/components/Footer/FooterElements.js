55aa414f5d1a4a4811beb6c863f60f97185ef8c1
Micah Sterling <msterling89@gmail.com>
2022-01-06 17:04:16 -0600

    add stlyed socailIcons and socailIconLink

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -97,3 +97,14 @@ export const WebsiteRights = styled.small`
   margin-bottom: 16px;
 `
 
+export const SocailIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`
+
+export const SocialIconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+`
