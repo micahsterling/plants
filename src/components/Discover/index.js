2e500f09a32142acd0d4c3a50ccf77e0f886c4f2
Micah Sterling <msterling89@gmail.com>
2022-01-22 13:10:47 -0600

    add google maps link

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -24,8 +24,8 @@ const Discover = () => {
           <Column1>
           <TextWrapper>
             <TopLine>Discover</TopLine>
-            <Heading lightText={false}>Terracotto</Heading>
-            <Subtitle darkText={true}>3056 W Diversey Ave Chicago, IL 600647</Subtitle>
+            <Heading lightText={false} >Terracotto Plants</Heading>
+            <Subtitle darkText={true} href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0" target="_blank"> <a href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0" target="_blank" rel="noreferrer">3056 W Diversey Ave Chicago, IL 600647 </a></Subtitle>
             <Subtitle darkText={true}>Hours:</Subtitle>
 
             <SubText darkText={true}>Monday: Closed </SubText> 
@@ -35,7 +35,6 @@ const Discover = () => {
             <SubText darkText={true}>Friday: 11am - 8pm </SubText> 
             <SubText darkText={true}>Saturday: 11am - 8pm </SubText> 
             <SubText darkText={true}>Sunday: 11am - 6pm </SubText> 
-    
     
           </TextWrapper>
           </Column1>
