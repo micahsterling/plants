bfeeb76d1ea41d817dc5fbcfaef6c9cad803951d
Micah Sterling <msterling89@gmail.com>
2022-01-24 13:24:23 -0600

    add about index

---
+++ src/components/About/Data.js
@@ -0,0 +1,17 @@
+
+export const about = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'About',
+  headLine: 'Terracotto Plants',
+  description: 'Im Cotto and this is my shop',
+  buttonLable: 'button',
+  imgStart: false,
+  img: require('../../images/img3.JPG').default,
+  alt: 'car',
+  dark: true,
+  primary: true,
+  darkText: false,
+}
---
+++ src/components/About/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import img from '../../images/img3.JPG'
+import {
+  InfoContainer, 
+  InfoWrapper, 
+  InfoRow, 
+  Column1, 
+  TextWrapper, 
+  TopLine, 
+  Heading, 
+  Subtitle, 
+  Column2, 
+  ImgWrap, 
+  Img,
+} from './AboutElements'
+
+const About = () => {
+  return (
+    <>
+      <InfoContainer lightBg={false} id='about'>
+        <InfoWrapper>
+          <InfoRow imgStart={true}>
+            <Column1>
+            <TextWrapper>
+              <TopLine>About</TopLine>
+              <Heading lightText={true}>Terracotto Plants</Heading>
+              <Subtitle darkText={false}>Coming Soon!!!</Subtitle>
+              <Subtitle darkText={false}>Learn more about the Shop</Subtitle>
+              {/* <BtnWrap>
+                <Button to='home'
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+                primary={primary ? 1 : 0}
+                dark={dark ? 1 : 0}
+                dark2={dark2 ? 1 : 0}>
+                  {buttonLable}
+                </Button>
+              </BtnWrap> */}
+            </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt='plants' />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
+  )
+}
+
+export default About
+
