4231ed2662db3f88e028ebc0b8422765b7570a05
Micah Sterling <msterling89@gmail.com>
2022-01-24 13:25:22 -0600

    update files

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -29,18 +29,15 @@ const Discover = () => {
             <Subtitle>
               <Link href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0" target="_blank" rel="noreferrer" darkText={true}>3056 W Diversey Ave Chicago, IL 600647 </Link>
             </Subtitle>
-
             
             <Subtitle darkText={true}>Hours:</Subtitle>
-
-            <SubText darkText={true}>Monday: Closed </SubText> 
-            <SubText darkText={true}>Tuesday: Closed </SubText> 
-            <SubText darkText={true}>Wednesday: 11am - 7pm </SubText> 
-            <SubText darkText={true}>Thursday: 11am - 7pm </SubText> 
-            <SubText darkText={true}>Friday: 11am - 8pm </SubText> 
-            <SubText darkText={true}>Saturday: 11am - 8pm </SubText> 
-            <SubText darkText={true}>Sunday: 11am - 6pm </SubText> 
-    
+              <SubText darkText={true}>Monday: Closed </SubText> 
+              <SubText darkText={true}>Tuesday: Closed </SubText> 
+              <SubText darkText={true}>Wednesday: 11am - 7pm </SubText> 
+              <SubText darkText={true}>Thursday: 11am - 7pm </SubText> 
+              <SubText darkText={true}>Friday: 11am - 8pm </SubText> 
+              <SubText darkText={true}>Saturday: 11am - 8pm </SubText> 
+              <SubText darkText={true}>Sunday: 11am - 6pm </SubText> 
           </TextWrapper>
           </Column1>
           <Column2>
