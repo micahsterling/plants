8275f683991b984b7024f7433d74f9195322e39e
Micah Sterling <msterling89@gmail.com>
2022-02-01 16:24:34 -0600

    realign hours

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -13,7 +13,8 @@ import {
   ImgWrap,
   Img,
   SubText,
-  Link
+  Link,
+  SubTextContainer
 } from './DiscoverElements'
 
 const Discover = () => {
@@ -29,15 +30,24 @@ const Discover = () => {
             <Subtitle>
               <Link href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0" target="_blank" rel="noreferrer" darkText={true}>3056 W Diversey Ave Chicago, IL 60647 </Link>
             </Subtitle>
-            
             <Subtitle darkText={true}>Hours:</Subtitle>
-              <SubText darkText={true}>Monday: Closed </SubText> 
-              <SubText darkText={true}>Tuesday: Closed </SubText> 
-              <SubText darkText={true}>Wednesday: 11am - 7pm </SubText> 
-              <SubText darkText={true}>Thursday: 11am - 7pm </SubText> 
-              <SubText darkText={true}>Friday: 11am - 8pm </SubText> 
-              <SubText darkText={true}>Saturday: 11am - 8pm </SubText> 
-              <SubText darkText={true}>Sunday: 11am - 6pm </SubText> 
+            <SubTextContainer>
+              <SubText darkText={true}>Monday: </SubText> 
+              <SubText darkText={true}>Closed </SubText> 
+              <SubText darkText={true}>Tuesday:</SubText> 
+              <SubText darkText={true}>Closed </SubText> 
+              <SubText darkText={true}>Wednesday:</SubText> 
+              <SubText darkText={true}>11am - 7pm </SubText> 
+              <SubText darkText={true}>Thursday: </SubText> 
+              <SubText darkText={true}>11am - 7pm </SubText> 
+              <SubText darkText={true}>Friday: </SubText> 
+              <SubText darkText={true}>11am - 8pm </SubText> 
+              <SubText darkText={true}>Saturday: </SubText> 
+              <SubText darkText={true}>11am - 8pm </SubText> 
+              <SubText darkText={true}>Sunday:</SubText> 
+              <SubText darkText={true}>11am - 6pm </SubText> 
+            </SubTextContainer>
+
           </TextWrapper>
           </Column1>
           <Column2>
