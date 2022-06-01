1f11588575fa16d592882184f45a677a65969a3c
Micah Sterling <msterling89@gmail.com>
2022-06-01 18:48:41 -0500

    add calendar to events

--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { TopLine, InfoRow, Column1, Column2, TextWrapper, Heading, Subtitle } from '../Discover/DiscoverElements';
 import {
   EventsContainer,
   EventsWrapper,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Calendar,
 } from './EventsElements'
 
 const Events = () => {
@@ -10,17 +13,13 @@ const Events = () => {
     <>
       <EventsContainer lightBg={true} id="events">
         <EventsWrapper>
-        <InfoRow >
-          <Column1>
           <TextWrapper>
             <TopLine>Events</TopLine>
             <Heading lightText={false} >Terracotto Events</Heading>
-            <Subtitle darkText={true}>Coming Soon</Subtitle>
           </TextWrapper>
-          </Column1>
-          <Column2>
-          </Column2>
-        </InfoRow>
+            <Calendar>
+            <iframe style={{ 'width': '100%', }} className='cal' title="Events Calendar" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%2333B679&ctz=America%2FChicago&showTitle=0&showPrint=0&showTabs=1&showCalendars=0&src=aHAyc3Nvc28xMWh2OWRoZXF0NmFwOGo5NzBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%230B8043&color=%23A79B8E" border={0} width={800} height={550} frameBorder={0} scrolling={"no"}></iframe>
+            </Calendar>
         </EventsWrapper>
       </EventsContainer>
     </>
