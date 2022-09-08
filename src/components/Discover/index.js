69f908b8780aeff2aba0ae11e75501e3120ce63b
Micah Sterling <msterling89@gmail.com>
2022-09-08 17:45:35 -0500

    update store hours

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import img from '../../images/storeFront.JPG'
+import React from "react";
+import img from "../../images/storeFront.JPG";
 import {
   DiscoverContainer,
   DiscoverWrapper,
@@ -15,50 +15,59 @@ import {
   SubText,
   ImgWrap,
   Img,
-} from './DiscoverElements'
+} from "./DiscoverElements";
 
 const Discover = () => {
   return (
     <>
-    <DiscoverContainer lightBg={true} id="discover">
-      <DiscoverWrapper>
-        <InfoRow >
-          <Column1>
-          <TextWrapper>
-            <TopLine>Discover</TopLine>
-            <Heading lightText={false} >Terracotto Plants</Heading>
-            <Subtitle>
-              <Link href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0" target="_blank" rel="noreferrer" darkText={true}>3056 W Diversey Ave Chicago, IL 60647 </Link>
-            </Subtitle>
-            <Subtitle  bold={true} darkText={true}>Hours:</Subtitle>
-            <SubTextContainer>
-              <SubText darkText={true}>Monday: </SubText> 
-              <SubText darkText={true}>Closed </SubText> 
-              <SubText darkText={true}>Tuesday:</SubText> 
-              <SubText darkText={true}>Closed </SubText> 
-              <SubText darkText={true}>Wednesday:</SubText> 
-              <SubText darkText={true}>12pm - 7pm </SubText> 
-              <SubText darkText={true}>Thursday: </SubText> 
-              <SubText darkText={true}>12pm - 7pm </SubText> 
-              <SubText darkText={true}>Friday: </SubText> 
-              <SubText darkText={true}>12pm - 8pm </SubText> 
-              <SubText darkText={true}>Saturday: </SubText> 
-              <SubText darkText={true}>11am - 8pm </SubText> 
-              <SubText darkText={true}>Sunday:</SubText> 
-              <SubText darkText={true}>11am - 6pm </SubText> 
-            </SubTextContainer>
-          </TextWrapper>
-          </Column1>
-          <Column2>
-            <ImgWrap>
-              <Img src={img} alt="Front of shop" />
-            </ImgWrap>
-          </Column2>
-        </InfoRow>
-      </DiscoverWrapper>
-    </DiscoverContainer>
-  </>
-  )
-}
+      <DiscoverContainer lightBg={true} id="discover">
+        <DiscoverWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>Discover</TopLine>
+                <Heading lightText={false}>Terracotto Plants</Heading>
+                <Subtitle>
+                  <Link
+                    href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0"
+                    target="_blank"
+                    rel="noreferrer"
+                    darkText={true}
+                  >
+                    3056 W Diversey Ave Chicago, IL 60647{" "}
+                  </Link>
+                </Subtitle>
+                <Subtitle bold={true} darkText={true}>
+                  Hours:
+                </Subtitle>
+                <SubTextContainer>
+                  <SubText darkText={true}>Monday: </SubText>
+                  <SubText darkText={true}>12pm - 6pm </SubText>
+                  <SubText darkText={true}>Tuesday:</SubText>
+                  <SubText darkText={true}>12pm - 7pm </SubText>
+                  <SubText darkText={true}>Wednesday:</SubText>
+                  <SubText darkText={true}>12pm - 7pm </SubText>
+                  <SubText darkText={true}>Thursday: </SubText>
+                  <SubText darkText={true}>12pm - 7pm </SubText>
+                  <SubText darkText={true}>Friday: </SubText>
+                  <SubText darkText={true}>12pm - 8pm </SubText>
+                  <SubText darkText={true}>Saturday: </SubText>
+                  <SubText darkText={true}>11am - 8pm </SubText>
+                  <SubText darkText={true}>Sunday:</SubText>
+                  <SubText darkText={true}>11am - 6pm </SubText>
+                </SubTextContainer>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt="Front of shop" />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </DiscoverWrapper>
+      </DiscoverContainer>
+    </>
+  );
+};
 
-export default Discover
+export default Discover;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { animateScroll as scroll } from 'react-scroll'
-import { FaInstagram, FaSpotify, FaYoutube } from 'react-icons/fa'
+import React from "react";
+import { animateScroll as scroll } from "react-scroll";
+import { FaInstagram, FaSpotify, FaYoutube } from "react-icons/fa";
 import {
-  FooterContainer, 
-  FooterWrap, 
-  FooterLinksContainer, 
-  FooterLinksWrapper, 
-  FooterLinkItems, 
-  FooterLinkTitle, 
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
   FooterLink,
   SocialMedia,
   SocialMediaWrap,
@@ -17,66 +17,80 @@ import {
   SocialIconLink,
   FooterText,
   FooterRightsContainer,
-} 
-  from './FooterElements'
+} from "./FooterElements";
 
 const Footer = () => {
   const toggleHome = () => {
     scroll.scrollToTop();
-  }
+  };
   return (
     <>
       <FooterContainer>
         <FooterWrap>
-              <FooterTitle to='/' onClick={toggleHome}>
-              Terracotto Plants
-              </FooterTitle>
+          <FooterTitle to="/" onClick={toggleHome}>
+            Terracotto Plants
+          </FooterTitle>
           <FooterLinksContainer>
             <FooterLinksWrapper>
               <FooterLinkItems>
-                <FooterLinkTitle>
-                  Address
-                </FooterLinkTitle>
-                  <FooterLink href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0" target="_blank" rel="noreferrer">
-                    3056 W Diversey Ave 
-                    <br />
-                    Chicago, IL 60647 
-                  </FooterLink>
+                <FooterLinkTitle>Address</FooterLinkTitle>
+                <FooterLink
+                  href="https://www.google.com/maps/dir//3056+W+Diversey+Ave,+Chicago,+IL+60647/@41.9322399,-87.7069416,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x880fcd7976e03411:0xf26f32d05783a912!2m2!1d-87.7047529!2d41.9322399!3e0"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  3056 W Diversey Ave
+                  <br />
+                  Chicago, IL 60647
+                </FooterLink>
               </FooterLinkItems>
               <FooterLinkItems>
-              <FooterLinkTitle>
-                  Store Hours
-                </FooterLinkTitle>
-                  <FooterText >Mon-Tue: Closed</FooterText>
-                  <FooterText >Wed-Thur: 12pm - 7pm</FooterText>
-                  <FooterText >Fri: 12pm - 8pm </FooterText>
-                  <FooterText >Sat: 11am - 8pm </FooterText>
-                  <FooterText >Sun: 11am - 6pm</FooterText>
+                <FooterLinkTitle>Store Hours</FooterLinkTitle>
+                <FooterText>Mon: 12pm - 6pm</FooterText>
+                <FooterText>Tue-Thur: 12pm - 7pm</FooterText>
+                <FooterText>Fri: 12pm - 8pm </FooterText>
+                <FooterText>Sat: 11am - 8pm </FooterText>
+                <FooterText>Sun: 11am - 6pm</FooterText>
               </FooterLinkItems>
             </FooterLinksWrapper>
           </FooterLinksContainer>
           <SocialMedia>
             <SocialMediaWrap>
               <SocailIcons>
-                <SocialIconLink href="//www.instagram.com/terracottoplants/" target="_blank" aria-label="Instagram">
+                <SocialIconLink
+                  href="//www.instagram.com/terracottoplants/"
+                  target="_blank"
+                  aria-label="Instagram"
+                >
                   <FaInstagram />
                 </SocialIconLink>
-                <SocialIconLink href="//open.spotify.com/show/4VsESgmTcgICKJ0xXKLlFk" target="_blank" aria-label="Spotify">
+                <SocialIconLink
+                  href="//open.spotify.com/show/4VsESgmTcgICKJ0xXKLlFk"
+                  target="_blank"
+                  aria-label="Spotify"
+                >
                   <FaSpotify />
                 </SocialIconLink>
-                <SocialIconLink href="//www.youtube.com/channel/UCvxkT72U71ItRoTnKnRVV1g/videos" target="_blank" aria-label="Youtube">
+                <SocialIconLink
+                  href="//www.youtube.com/channel/UCvxkT72U71ItRoTnKnRVV1g/videos"
+                  target="_blank"
+                  aria-label="Youtube"
+                >
                   <FaYoutube />
                 </SocialIconLink>
               </SocailIcons>
             </SocialMediaWrap>
           </SocialMedia>
           <FooterRightsContainer>
-            <WebsiteRights>© {new Date().getFullYear()} Terracotto Plants. All rights reserved. </WebsiteRights>
+            <WebsiteRights>
+              © {new Date().getFullYear()} Terracotto Plants. All rights
+              reserved.{" "}
+            </WebsiteRights>
           </FooterRightsContainer>
         </FooterWrap>
       </FooterContainer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
